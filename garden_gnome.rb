a989114991fa74604cdaabdeda8ca6c9713c0d35
Paigeboyce <boycepaige1@gmail.com>
2018-06-21 21:18:48 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,19 @@
 # Code your instances here
+
+class GardenGnome
+  attr_accessor :name, :age, :gluten_allergy,:personality
+def initialize(name,age,gluten_allergy,personality)
+
+  @name = name
+  @age = age
+  @gluten_allergy = gluten_allergy
+  @personality = personality
+ end
+
+end
+GardenGnome1 = GardenGnome.new("Paige", 4000)
+GardenGnome2 = GardenGnome.new("Emma", 4000)
+
+puts @personality = "evil"
+
+class hat_color
